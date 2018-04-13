f7d3aee636bcec63e1a6b6c2fe472c8ac70be54d
Lijie Huang <huanglijie@outlook.com>
2018-04-14 00:10:02 +0800

    renew melt script

--- workbench/stimuli/adjust_frame_rate.sh
+++
@@ -1,9 +0,0 @@
-ffmpeg -y -i fg_av_seg0.mkv -r 25 fg_av_seg0_25fps.mkv
-ffmpeg -y -i fg_av_seg1.mkv -r 25 fg_av_seg1_25fps.mkv
-ffmpeg -y -i fg_av_seg2.mkv -r 25 fg_av_seg2_25fps.mkv
-ffmpeg -y -i fg_av_seg3.mkv -r 25 fg_av_seg3_25fps.mkv
-ffmpeg -y -i fg_av_seg4.mkv -r 25 fg_av_seg4_25fps.mkv
-ffmpeg -y -i fg_av_seg5.mkv -r 25 fg_av_seg5_25fps.mkv
-ffmpeg -y -i fg_av_seg6.mkv -r 25 fg_av_seg6_25fps.mkv
-ffmpeg -y -i fg_av_seg7.mkv -r 25 fg_av_seg7_25fps.mkv
-
